c5d197a6e11eb833f2922855911db189dc25f2e3
ksork <ksork@yandex.ru>
2014-08-14 13:29:50 +0600

    переписан TariffActivity

--- src/com/example/UTM5_info/TariffsActivity.java
+++ src/com/example/UTM5_info/TariffsActivity.java
@@ -2,6 +2,7 @@ package com.example.UTM5_info;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,11 +18,17 @@ import java.util.Collections;
 public class TariffsActivity extends Activity{
 
     private final String LOG_TAG = "Tariff_log";
-    private ArrayList tariffData;
-    private ListView lvTariffs;
     private Context context;
     private final String[] TARIFFS_ID = {"8", "7", "9", "10"};
     private final int[] TARIFFS_PRICE = {400, 500, 600, 800};
+    private ArrayList tariffData;
+    private  CheckBox[] checkBoxes = new CheckBox[4];
+
+    private ListView lvTariffs;
+    private CheckBox cbTariff1;
+    private CheckBox cbTariff2;
+    private CheckBox cbTariff3;
+    private CheckBox cbTariff4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,25 +37,25 @@ public class TariffsActivity extends Activity{
 
         context = getApplicationContext();
         lvTariffs = (ListView) findViewById(R.id.lvTariffs);
+        cbTariff1 = (CheckBox) findViewById(R.id.cbTariff1);
+        cbTariff2 = (CheckBox) findViewById(R.id.cbTariff2);
+        cbTariff3 = (CheckBox) findViewById(R.id.cbTariff3);
+        cbTariff4 = (CheckBox) findViewById(R.id.cbTariff4);
+        checkBoxes[0] = cbTariff1;
+        checkBoxes[1] = cbTariff2;
+        checkBoxes[2] = cbTariff3;
+        checkBoxes[3] = cbTariff4;
+
         tariffData = MainActivity.user.getTariffList();
         // Меняем тарифы "Оптима" и "Актив" местами
         Collections.swap(tariffData, 1, 2);
-        //showTariffs();
         }
 
-    // Выводим список тарифов на экран
-    private void showTariffs(){
-        String[] from = {"tariffName", "tariffDescription", "tariffEnable"};
-        int[] to = {R.id.tvTariffName, R.id.tvTariffDescription, R.id.imgGalka};
-        SimpleAdapter adapter = new SimpleAdapter(this, tariffData, R.layout.tariffs_list_item, from, to);
-        lvTariffs.setAdapter(adapter);
-        lvTariffs.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-    }
 
     // Проверка количества бабла, нужного для перехода на выбраный тариф
-    private boolean isBalanceLow(int tariffSelected){
+    private boolean isBalanceLow(int tariffSelect){
         int balance = Integer.parseInt(MainActivity.user.getBalance());
-        int tariffPrice = TARIFFS_PRICE[tariffSelected]+1;
+        int tariffPrice = TARIFFS_PRICE[tariffSelect];
         return (balance < tariffPrice);
     }
 
@@ -58,16 +65,28 @@ public class TariffsActivity extends Activity{
             setResult(1);
             return;
         }
-        int tariffSelected = lvTariffs.getCheckedItemPosition();
-        if (tariffSelected == -1){return;}
+
+        int tariffSelect;
+        if(cbTariff1.isChecked()) tariffSelect = 0;
+        else if(cbTariff2.isChecked()) tariffSelect = 1;
+        else if(cbTariff3.isChecked()) tariffSelect = 2;
+        else if(cbTariff4.isChecked()) tariffSelect = 3;
+        else return;
         // Проверяем баланс
-        if (isBalanceLow(tariffSelected)){
+        if (isBalanceLow(tariffSelect)){
             Dialog.showMessage(TariffsActivity.this, "Недостаточно денег !", "Для перехода на выбраный тариф " +
-                    "необходим баланс более " + TARIFFS_PRICE[tariffSelected] + "руб.");
+                    "необходим баланс более " + TARIFFS_PRICE[tariffSelect] + "руб.");
             return;
         }
-        MainActivity.user.changeTariff(TARIFFS_ID[tariffSelected]);
+        MainActivity.user.changeTariff(TARIFFS_ID[tariffSelect]);
         setResult(RESULT_OK);
         finish();
+    }
+
+    // Снимаем галочки с не выбранных чекбоксов
+    public void onCbClick(View v) {
+        for (CheckBox cb : checkBoxes) {
+            if (cb.getId() != v.getId()) cb.setChecked(false);
+        }
     }
 }
